b48e80d87432a6b92d96688f7db6798b05ca59c0
cklynch2 <cklynch2@gmail.com>
2016-05-11 13:35:44 -0400

    Conference badges with first and last name solution

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,11 +8,17 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+//    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+//    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+//        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+//    }
+    
+    NSArray *conferenceSpeakersLastName = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    NSArray *conferenceSpeakersFirstName = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    for (NSUInteger i = 0; i < [conferenceSpeakersLastName count]; i++) {
+        NSLog(@"Hello, my name is %@ %@.", conferenceSpeakersFirstName[i],conferenceSpeakersLastName[i]);
+    }
+
     
     // do not alter
     return YES;  //
